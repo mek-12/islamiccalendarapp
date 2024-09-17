e442aacb0b9bf18a8679fc9a3da85dfff0ff0ff6
Muhammet Emin Karaboğa <mek.krbg04@gmail.com>
2024-09-17 10:06:20 +0300

    proxy added

--- IslamicCalendarApp/Program.cs
+++ IslamicCalendarApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -6,8 +7,22 @@ using Newtonsoft.Json;
 class Program {
     static async Task Main(string[] args) {
         string url = "http://api.aladhan.com/v1/gToHCalendar/8/2024";
+        bool useProxy = true;
+        string proxyUrl = "http://your-proxy-url:port";
 
-        using (HttpClient client = new HttpClient()) {
+        HttpClientHandler handler;
+
+        if (useProxy) {
+            handler = new HttpClientHandler {
+                Proxy = new WebProxy(proxyUrl),
+                UseProxy = true
+            };
+        } else {
+            handler = new HttpClientHandler {
+                UseProxy = false
+            };
+        }
+        using (HttpClient client = new HttpClient(handler)) {
             HttpResponseMessage response = await client.GetAsync(url);
 
             if (response.IsSuccessStatusCode) {
